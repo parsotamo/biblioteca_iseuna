ffc72b43f36dcd73cb072c6a49ee61e05a60a129
Ilan Parso <ilanparso@Ilans-MacBook-Air.local>
2023-02-21 12:41:47 +0200

    biblioteca no Vercel

--- base_dados_con.php
+++ base_dados_con.php
@@ -1,7 +1,11 @@
 <?php
 
 //base_dados_con.php
+$db_usuario = "b2230ba974860a";
+$db_senha = "443fe718";
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_nome = "heroku_152f715103496d7";
 
-$connect = new PDO("mysql:host=localhost:3306; dbname=db_iseuna", "root", "ilpjsc94@");
+$connect = new PDO($db_host . ';' . 'dbname=' . $db_nome, $db_usuario, $db_senha);
 
 session_start();
