6429587de2a7cd7b3f21a5a2f12bc297f8a67753
Ilan Parso <ilanparso@Ilans-MacBook-Air.local>
2023-02-21 22:09:30 +0200

    biblioteca no Heroku

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '2d869dcc6348e3e515c3cad82ebda57539cfdb31',
+        'reference' => 'ae2723110c039ec6453da1fd509cfb0a40e8df85',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '2d869dcc6348e3e515c3cad82ebda57539cfdb31',
+            'reference' => 'ae2723110c039ec6453da1fd509cfb0a40e8df85',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
