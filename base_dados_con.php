1c96b20caa5d89ad676b96b461b7c3028b3f9066
Ilan Parso <ilanparso@Ilans-MacBook-Air.local>
2023-02-21 14:34:09 +0200

    Adicionando Biblioteca no Heroku

--- base_dados_con.php
+++ base_dados_con.php
@@ -6,6 +6,6 @@ $db_senha = "443fe718";
 $db_host = "us-cdbr-east-06.cleardb.net";
 $db_nome = "heroku_152f715103496d7";
 
-$connect = new PDO($db_host . ';' . 'dbname=' . $db_nome, $db_usuario, $db_senha);
+$connect = new PDO('mysql://' . $db_host . ';' . 'dbname=' . $db_nome, $db_usuario, $db_senha);
 
 session_start();
