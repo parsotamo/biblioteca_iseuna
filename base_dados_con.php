faf46b28290a9d34ba016860826e8c0f9ec209c0
Ilan Parso <ilanparso@Ilans-MacBook-Air.local>
2023-02-23 23:04:25 +0200

    biblioteca no Heroku

--- base_dados_con.php
+++ base_dados_con.php
@@ -6,10 +6,11 @@ require 'vendor/autoload.php';
 $dotenv = Dotenv\dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-if ($_ENV["DEV"]) {
+echo getenv("DB_USUARIO");
+if (getenv("DEV")) {
     $connect = new PDO("mysql:host=localhost:3306;dbname=db_iseuna", "root", "123");
 } else {
-    $connect = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NOME'], $_ENV['DB_USUARIO'], $_ENV['DB_SENHA']);
+    $connect = new PDO('mysql:host=' . getenv("DB_HOST") . ';dbname=' . getenv("DB_NOME"), getenv("DB_USUARIO"), getenv("DB_SENHA"));
 }
 
 session_start();
